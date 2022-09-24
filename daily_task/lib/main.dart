137f0b04fed8000aff136067116358c258c150dc
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-24 21:23:16 +0530

    done modification in the UI

--- daily_task/lib/main.dart
+++ daily_task/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'Pages/Home.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
